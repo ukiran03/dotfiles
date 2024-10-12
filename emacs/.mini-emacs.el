02d6cfe252f93ad235c673d7bd963cb9fac5f455
ukiran03 <ushakiranreddi@gmail.com>
2024-10-12 10:42:38 +0530

    Minor changes
    deleted:    emacs/.emacs.d/projects
    modified:   emacs/.gitignore
    modified:   emacs/.mini-emacs.el
    modified:   gui-apps/.gitignore
    modified:   gui-apps/.local/share/applications/nsxiv-dir.desktop
    modified:   xorg/.Xresources

--- emacs/.mini-emacs.el
+++ emacs/.mini-emacs.el
@@ -82,8 +82,8 @@
 
 ;; (unless (eq window-system 'ns)
 ;;   (menu-bar-mode -1))
-;; (when (fboundp 'tool-bar-mode)
-;;   (tool-bar-mode -1))
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
 ;; (when (fboundp 'horizontal-scroll-bar-mode)
