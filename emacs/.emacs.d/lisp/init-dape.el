84c4add70a50811d6c7bc9c6dd1fa9e7b97a0509
ukiran03 <ushakiranreddi@gmail.com>
2026-03-18 17:11:18 +0530

    emacs: configure dape with delve for go support, finally

--- emacs/.emacs.d/lisp/init-dape.el
+++ emacs/.emacs.d/lisp/init-dape.el
@@ -1,96 +1,37 @@
 (use-package dape
   :ensure t
   :init
-  (setq
-   ;; Performance tweaks
-   gc-cons-threshold 80000000
-
-   dape-buffer-window-arrangement 'right
-   dape-inlay-hints-variable-name-max 50
-   dape-inlay-hints nil
-
-   ;; TODO try using :autoport instead of 55878
-   dape-configs
-   '(
-     (go-test
-      modes (go-mode go-ts-mode)
-      command "dlv"
-      command-args ("dap" "--listen" "127.0.0.1:55878")
-      command-cwd default-directory
-      host "127.0.0.1"
-      port 55878
-      :name "Run the go tests in cwd"
-      :request "launch"
-      :mode "test"
-      :type "go"
-      :program "."
-      )
-
-     ;; (go-run-main
-     ;;  modes (go-mode go-ts-mode)
-     ;;  command "dlv"
-     ;;  command-args ("dap" "--listen" "127.0.0.1:55878")
-     ;;  ;; command-cwd (project-root (project-current))
-     ;;  command-cwd default-directory
-     ;;  host "127.0.0.1"
-     ;;  port 55878
-     ;;  :name "Run main.go with --localtest"
-     ;;  :request "launch"
-     ;;  :mode "debug"
-     ;;  :type "go"
-     ;;  :program "main.go"
-     ;;  :args ["-localtest"]
-     ;;  )
-     (go-run-main
-      modes (go-mode go-ts-mode)
-      command "dlv"
-      command-cwd default-directory
-      :name "Run main.go with --localtest"
-      :request "launch"
-      :mode "debug"
-      :type "go"
-      :program "main.go"
-      :args ["-localtest"]
-      )
-     (go-devspace
-      modes (go-mode go-ts-mode)
-      host "127.0.0.1"
-      port 2345
-      prefix-local (lambda () default-directory)
-      prefix-remote "/workspace/"
-      :name "Debug remote in devspace pod"
-      :request "attach"
-      :mode "remote"
-      :type "go"
-      command "dlv"
-      command-args ("connect" "127.0.0.1:2345")
-      )
-
-     (go-attach
-      host "127.0.0.1"
-      ;; This is the port exposed in this command:
-      ;;
-      ;; dlv attach $(lsof -i TCP -s TCP:LISTEN -nP | awk '/:3000/ { print $2 }') --headless --listen=127.0.0.1:5005
-      ;; dlv attach 68436 --headless --listen=127.0.0.1:38697 --accept-multiclient --api-version=2 --log
-      port 5005
-      :name "Attach to running Go process"
-      :modes (go-mode go-ts-mode)
-      :type "go"
-      :request "attach"
-      :mode "remote"
-      :cwd (project-root (project-current))
-      command "dlv"
-      command-args ("connect" "127.0.0.1:5005")
-      )
-     ))
+  ;; Performance tweaks
+  (setq gc-cons-threshold 80000000)
 
   :config
+  (setq dape-buffer-window-arrangement 'right)
+  (setq dape-inlay-hints-variable-name-max 50)
+  (setq dape-inlay-hints nil)
+  :config
+  ;; Pulse source line (performance hit)
   (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+  ;; To not display info and/or buffers on startup
+  ;; (remove-hook 'dape-start-hook 'dape-info)
+  (remove-hook 'dape-start-hook 'dape-repl)
 
-  ;; (global-set-key [f7] 'dape-step-in)
-  ;; (global-set-key [f8] 'dape-next)
-  ;; (global-set-key [f9] 'dape-continue)
-  )
+  :config
+  ;; Add the Go delve configuration if not already present
+  (add-to-list 'dape-configs
+               `(go-debug-main
+                 modes (go-mode go-ts-mode)
+                 command "dlv"
+                 command-args ("dap" "--listen" "127.0.0.1::autoport")
+                 command-cwd dape-command-cwd
+                 port :autoport
+                 :type "debug"
+                 :request "launch"
+                 :name "Debug Go Program"
+                 :cwd "."
+                 :program "."
+                 :args [])))
+
+
 
 
 (provide 'init-dape)
