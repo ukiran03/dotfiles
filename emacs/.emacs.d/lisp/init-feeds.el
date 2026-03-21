2d6db7895ff754c33d48cda92c6c01a8db64e0a8
ukiran03 <ushakiranreddi@gmail.com>
2026-03-21 10:12:23 +0530

    emacs: added elfeed settings, elfeed-org for feeds

--- emacs/.emacs.d/lisp/init-feeds.el
+++ emacs/.emacs.d/lisp/init-feeds.el
@@ -1,9 +1,29 @@
 
 ;; <https://github.com/skeeto/elfeed>
-(use-package elfeed :ensure nil)
+(use-package elfeed
+  :bind (:map elfeed-show-mode-map
+              ("V" . 'my-elfeed-mpv-play))
+  :config
+  (setq elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory)
+        elfeed-show-entry-switch 'display-buffer
+        elfeed-feeds nil
+        elfeed-search-filter "@6-months-ago +unread -fun")
+
+  (defun my-elfeed-mpv-play ()
+    "Play the current elfeed entry with mpv."
+    (interactive)
+    (let ((link (elfeed-entry-link elfeed-show-entry)))
+      (when link (start-process "elfeed-mpv" nil "mpv" link)))))
 
 ;; <https://github.com/remyhonig/elfeed-org>
-(use-package elfeed-org :ensure nil)
+;; Configure Elfeed with org mode
+(use-package elfeed-org
+  :ensure t
+  :init
+  (setq rmh-elfeed-org-files
+        (list (expand-file-name "feeds.org" org-directory)))
+  :config
+  (elfeed-org))
 
 ;; <https://github.com/karthink/elfeed-tube>
 (use-package elfeed-tube :ensure nil)
@@ -13,3 +33,5 @@
 
 ;; <https://github.com/Manoj321/elfeed-dashboard>
 (use-package elfeed-dashboard :ensure nil)
+
+(provide 'init-feeds)
