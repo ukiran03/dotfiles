fa4dda4dfd27f59a6797afb5dd7f94efe9ced391
ukiran03 <ushakiranreddi@gmail.com>
2026-03-27 14:03:29 +0530

    refactor[emacs]: re-writing golang settings, formatters
    My Quest TODO:
    How to seamlessly join the functionality of
    `eglot-code-action-organize-imports` and `apheleia-mode`

--- emacs/.emacs.d/lisp/init-go.el
+++ emacs/.emacs.d/lisp/init-go.el
@@ -1,84 +1,136 @@
-;; init-go.el --- Initialize Golang configurations.	-*- lexical-binding: t -*-
+(use-package go-mode
+  :ensure t
+  :display "Go Utility Tools"
+  :commands (godef-jump
+             godef-describe
+             go-import-add
+             go-run
+             go-goto-arguments
+             go-goto-docstring
+             go-goto-function
+             go-goto-imports
+             go-goto-method-receiver
+             go-goto-function-name
+             go-goto-return-values))
+
+(defun my/golang-init-eglot ()
+  "Start eglot only if a go.mod exists in the project root."
+  (let* ((proj (project-current))
+         (root (when proj (project-root proj))))
+    (when (and root (file-exists-p (expand-file-name "go.mod" root)))
+      (eglot-ensure))))
+
+;; (defun my/golang-init-eglot ()
+;;   "Start Eglot if go.mod exists; otherwise, fix Apheleia for scratch files."
+;;   (let* ((proj (project-current))
+;;          (root (when proj (project-root proj))))
+;;     (if (and root (file-exists-p (expand-file-name "go.mod" root)))
+;;         ;; CASE 1: Project found
+;;         (eglot-ensure)
+
+;;       ;; CASE 2: Scratch file (No go.mod)
+;;       ;; We manually add goimports back to the chain for THIS buffer only.
+;;       (setq-local apheleia-mode-alist
+;;                   (append '((go-ts-mode . (goimports uk-go-fmt)))
+;;                           apheleia-mode-alist)))))
+
+;; (defun my/go-organize-imports-on-save ()
+;;   "Safely organize imports via Eglot for Go buffers."
+;;   (when (derived-mode-p 'go-ts-mode)
+;;     (add-hook 'before-save-hook
+;;               (lambda ()
+;;                 (interactive)
+;;                 (ignore-errors (eglot-code-action-organize-imports
+;;                                 (point-min) (point-max)))) nil t)
+
+;;     ;; ;; 2. The Apheleia Logic (Buffer-local)
+;;     ;; ;; We make this change buffer-local so it doesn't affect other Go files
+;;     ;; (setq-local apheleia-mode-alist
+;;     ;;             (copy-alist apheleia-mode-alist))
+;;     ;; (setf (alist-get 'go-ts-mode apheleia-mode-alist) 'go-format)
+;;     ))
 
 (use-package go-ts-mode
   :ensure nil
-  :mode ("\\.go\\'" . go-ts-mode)
-  :bind (:map go-ts-mode-map
-              ("<f1>" . compile))
-  :interpreter ("go" . go-ts-mode)
-  :hook ((go-ts-mode . apheleia-mode)
-         (go-ts-mode . symbol-overlay-mode)
+  :mode "\\.go\\'"
+  :interpreter "go"
+  :hook ((go-ts-mode . symbol-overlay-mode)
+         (go-ts-mode . apheleia-mode) ; TODO:
          (go-ts-mode . my/disable-super-save)
-         (go-ts-mode . (lambda ()
-                         (let* ((proj (project-current))
-                                (root (when proj (project-root proj))))
-                           (when (and root
-                                      (file-exists-p (expand-file-name "go.mod" root))
-                                      (not (bound-and-true-p eglot--managed-mode)))
-                             (eglot-ensure))))))
+         (go-ts-mode . my/golang-init-eglot)
+         ;; Attach the import-organizer only when Eglot is actually active
+         ;; (eglot-managed-mode-hook . my/go-organize-imports-on-save)
+         )
+
+  :bind (:map go-ts-mode-map
+              ("<f1>"          . compile)
+              ("C-M-q"         . prog-indent-sexp)
+              ("M-q"           . prog-fill-reindent-defun)
+              ;; Navigation & Refactoring (Powered by go-mode)
+              ("H-c a"         . go-import-add)
+              ("H-c d"         . godef-describe)
+              ("H-c j"         . godef-jump)
+              ("H-c r"         . go-run)
+              ("C-x 4 C-c C-j" . godef-jump-other-window)
+              ;; "Go-to" Helpers
+              ("H-c f a"       . go-goto-arguments)
+              ("H-c f d"       . go-goto-docstring)
+              ("H-c f f"       . go-goto-function)
+              ("H-c f i"       . go-goto-imports)
+              ("H-c f m"       . go-goto-method-receiver)
+              ("H-c f n"       . go-goto-function-name)
+              ("H-c f r"       . go-goto-return-values))
+
   :config
   (setq go-ts-mode-indent-offset 4)
-  (with-eval-after-load 'exec-path-from-shell
-    (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY")))
 
-  ;; Hook it up to both standard and Tree-sitter Go modes
-  (use-package go-mode
-    :ensure t)
-  :bind (:map go-ts-mode-map
-              ("H-c a" . go-import-add)
-              ("H-c d" . godef-describe)
-              ("H-c j" . godef-jump)
-              ("H-c r" . go-run)
-              ("H-c f a" . go-goto-arguments)
-              ("H-c f d" . go-goto-docstring)
-              ("H-c f f" . go-goto-function)
-              ("H-c f i" . go-goto-imports)
-              ("H-c f m" . go-goto-method-receiver)
-              ("H-c f n" . go-goto-function-name)
-              ("H-c f r" . go-goto-return-values)
-              ("C-x 4 C-c C-j" . godef-jump-other-window)
-              ("C-M-q"   . prog-indent-sexp)
-              ("M-q"     . prog-fill-reindent-defun)))
+  ;; Sync environment variables if the package is available
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-envs '("GOPATH" "GO111MODULE" "GOPROXY"))))
 
 (add-to-list 'major-mode-remap-alist '(go-mode . go-ts-mode))
 
 (use-package apheleia
   :config
-  ;; go install mvdan.cc/gofumpt@latest
-  (setf (alist-get 'uk-go-fmt apheleia-formatters)
+  (setf (alist-get 'go-format apheleia-formatters)
         '("golines" "--base-formatter" "gofumpt -extra"))
-  (setf (alist-get 'gotmplfmt apheleia-formatters)
-        '("gotmplfmt"))
-  (setf (alist-get 'templ-fmt apheleia-formatters)
-        '("templ" "fmt"))
+  (setf (alist-get 'gotmplfmt apheleia-formatters) '("gotmplfmt"))
+  (setf (alist-get 'templ-fmt apheleia-formatters) '("templ" "fmt"))
 
-  (setf (alist-get 'go-mode apheleia-mode-alist)
-        '(goimports uk-go-fmt))
-  (setf (alist-get 'go-ts-mode apheleia-mode-alist)
-        '(goimports uk-go-fmt))
+  (setf (alist-get 'go-mode apheleia-mode-alist) '(goimports go-format))
+  (setf (alist-get 'go-ts-mode apheleia-mode-alist) '(goimports go-format))
+  ;; (setf (alist-get 'go-mode apheleia-mode-alist) 'go-format)
+  ;; (setf (alist-get 'go-ts-mode apheleia-mode-alist) 'go-format)
+
   (setf (alist-get 'web-mode apheleia-mode-alist) '(gotmplfmt))
   (setf (alist-get 'templ-ts-mode apheleia-mode-alist) '(templ-fmt)))
 
 (use-package templ-ts-mode
   :mode ("\\.templ\\'" . templ-ts-mode)
-  :hook ((templ-ts-mode . apheleia-mode)))
+  :hook ((templ-ts-mode . apheleia-mode)
+         (templ-ts-mode . emmet-mode)))
 
-(use-package gotest
-  :ensure t)
-;; (use-package go-test
-;;   :bind (:map go-mode-map
-;;               ("C-c t r" . go-test-current-test)
-;;               ("C-c t f" . go-test-current-file))
+;; (use-package gotest
+;;   :bind (:map go-keymap
+;;               ("C-c t f" . go-test-current-file)
+;;               ("C-c t t" . go-test-current-test)
+;;               ("C-c t j" . go-test-current-project)
+;;               ("C-c t b" . go-test-current-benchmark)
+;;               ("C-c t c" . go-test-current-coverage)
+;;               ("C-c t x" . go-run)))
 
-;; go install github.com/josharian/impl@latest
 (use-package go-impl
   :ensure t)
 
-;; go install github.com/fatih/gomodifytags@latest
 (use-package go-tag
   :ensure t)
 
-;; go get -u github.com/davidrjenni/reftools/cmd/fillstruct
 (use-package go-fill-struct)
 
 (provide 'init-go)
+
+;;;;;; Go Tools
+;; go install mvdan.cc/gofumpt@latest
+;; go install github.com/josharian/impl@latest
+;; go install github.com/fatih/gomodifytags@latest
+;; go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
