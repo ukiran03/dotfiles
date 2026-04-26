378727af82f848cee4ae22fa594971a6a4b4b8c3
ukiran03 <ushakiranreddi@gmail.com>
2026-04-26 14:00:17 +0530

    emacs: revert elfeed settings, but keeping funcs as they
    Need to write some Elisp to make using elfeed a better experience

--- emacs/.emacs.d/lisp/init-feeds.el
+++ emacs/.emacs.d/lisp/init-feeds.el
@@ -3,17 +3,31 @@
 (use-package elfeed
   :bind (:map elfeed-show-mode-map
               ("V" . 'my-elfeed-mpv-play))
+  :hook ((elfeed-show-mode . visual-line-mode)
+         ;; (elfeed-show-mode . olivetti-mode)
+         )
   :config
   (setq elfeed-db-directory (expand-file-name "elfeed" user-emacs-directory)
         elfeed-show-entry-switch 'display-buffer
         elfeed-feeds nil
-        elfeed-search-filter "@6-months-ago +unread -fun")
+        elfeed-search-filter "@6-months-ago +unread -trash -fun -hn"
+        ;; elfeed-show-entry-switch #'elfeed-display-buffer ;; Not working
+        )
 
   (defun my-elfeed-mpv-play ()
     "Play the current elfeed entry with mpv."
     (interactive)
     (let ((link (elfeed-entry-link elfeed-show-entry)))
-      (when link (start-process "elfeed-mpv" nil "mpv" link)))))
+      (when link (start-process "elfeed-mpv" nil "mpv" link))))
+
+  (defun elfeed-display-buffer (buf &optional act)
+    (pop-to-buffer buf)
+    (set-window-text-height (get-buffer-window) (round (* 0.5 (frame-height)))))
+
+  (defun uk-elfeed-open ()
+    "Wrapper to load the elfeed db from disk before opening"
+    (interactive)
+    (elfeed-db-load) (elfeed) (elfeed-search-update--force)))
 
 ;; <https://github.com/remyhonig/elfeed-org>
 ;; Configure Elfeed with org mode
